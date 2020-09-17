1470d685b6a8add64a4ea14ea3c21cb774d2dae6
PalPalash <64191803+PalPalash@users.noreply.github.com>
2020-09-17 12:44:26 +0530

    Update and rename pip-modules-installer.bat to pip-modules-installer.py

--- pip-modules-installer.bat
+++
@@ -1,5 +0,0 @@
-pip install pandas 
-pip install numpy
-pip install pickle-mixin 
-pip install sklearn
-pip install flask 
---
+++ pip-modules-installer.py
@@ -0,0 +1,7 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+py -m pip install pandas 
+py -m pip install numpy
+py -m pip install pickle-mixin 
+py -m pip install sklearn
+py -m pip install flask 
