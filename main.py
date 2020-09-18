b888ee0b507406c33d20541d9e65ccba50fd0c6c
Aarni Koskela <akx@iki.fi>
2020-09-18 10:25:42 +0300

    Fix main.py to do something useful

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pickle
 from flask import Flask
+
 app = Flask(__name__)
 
 # open a file, where you stored the pickled data
-file = open('model.pkl', 'rb')
-clf = pickle.load(file)
-file.close()
+with open('model.pkl', 'rb') as file:
+    clf = pickle.load(file)
+
 
 @app.route('/')
 def hello_world():
     # Code For Infrence
-    inputFeatures = [100,1,26,1,0]
+    inputFeatures = [100, 1, 26, 1, 0]
     infProb = clf.predict_proba([inputFeatures])[0][1]
-    return 'Hello, World!' + str(infProb)
+    return repr({'input': inputFeatures, 'output': infProb})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
