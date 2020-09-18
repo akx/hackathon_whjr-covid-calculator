06eb51554e47a1f0cea63c6c19d34da47f6767e5
Aarni Koskela <akx@iki.fi>
2020-09-18 10:24:27 +0300

    Fix myTraining.py

--- myTraining.py
+++ myTraining.py
@@ -1,36 +1,26 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-import pickle 
+from sklearn.model_selection import train_test_split
+import pickle
 
 if __name__ == "__main__":
+    # Read data
     df = pd.read_csv('data.csv')
- 
-def data_split(data,ratio):
-    np.random.seed(42)
-    shuffled = np.random.permutation(len(data))
-    test_set_size = int(len(data)* ratio )
-    test_indeces = shuffled[:test_set_size]
-    train_indeces = shuffled[test_set_size:]
-    return data.iloc[train_indeces], data.iloc[test_indeces]
 
-if __name__ == "__main__":
-    # Read The Data
-    df = pd.read_csv('data.csv')
-    train ,test =  data_split(df, 0.2)
-    
-    X_train = train[[' fever' , ' bodyPain' , ' age' , ' runnyNose' , ' diffBreath' ]].to_numpy()
-    X_test = test[[' fever' , ' bodyPain' , ' age' , ' runnyNose' , ' diffBreath' ]].to_numpy()
-    
-    Y_train = train[[' infProb']].to_numpy().reshape(800,)
-    Y_test = test[[' infProb']].to_numpy().reshape(199,) 
+    X = df[['fever', 'bodyPain', 'age', 'runnyNose', 'diffBreath']].to_numpy()
+    y = df[['infProb']].to_numpy()
 
-    # open a file, where you ant to store the data
-    file = open('model.pkl', 'wb')
-    # dump information to that file
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+    # Train model
     clf = LogisticRegression()
-    pickle.dump(clf, file)
-    clf.fit(X_train,Y_train)
-    file.close()
+    clf.fit(X_train, y_train.ravel())
 
-    
+    # Test model
+    print('Score:', clf.score(X_test, y_test.ravel()))
+
+    # Save model
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(clf, f)
